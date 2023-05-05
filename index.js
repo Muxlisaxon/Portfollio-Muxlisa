bac18bdb260ebdb8a22affe8f67d8acbe1cb9096
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-05-05 19:17:32 +0500

    h

---
+++ index.js
@@ -0,0 +1,18 @@
+// let btn = document.querySelector(".btn")
+// let count = document.querySelector(".count")
+// let words = document.querySelector(".words")
+
+
+
+
+
+
+
+
+
+// function charCount(textarea) {
+//     var length = textarea.value.length;
+//     count = text.length
+// }
+
+
